71671b4b109fe4024f4673ed52e0aa17968ea8c1
dhelms33 <derrickhelms10@gmail.com>
2025-04-04 13:17:24 -0600

    Added random

---
+++ 03_Random.py
@@ -0,0 +1,31 @@
+import random
+
+#to see methods avaliable we should print dir
+
+print(dir(random))
+
+print(help(random.random))
+
+#random(...) method of random.Random instance
+    #random() -> x in the interval[0,1) can never return 1 can return 0
+
+#display 10 random numbers from interval [0,1)
+
+class Rando:
+    def ten_random():
+        numbers = []
+        for i in range(10):
+            num =  print(random.random())
+            numbers.append(num)
+        return numbers
+    def scale_random():
+        numbers_scaled = []
+        #Scales a random number
+        for i in range(10):
+            nums = 4 * random.random() + 3
+            numbers_scaled.append(nums)
+        return numbers_scaled
+
+if __name__ == '__main__':
+    random_instance = Rando()
+    print(random_instance.ten_random())
