87304dad989de1730d3d24cd57b97bc70f44b79d
dhelms33 <derrickhelms10@gmail.com>
2025-04-10 14:42:02 -0600

    Added time and format

--- 04_logging.py
+++ 04_logging.py
@@ -6,18 +6,19 @@
 import logging
 #allows us to look inside of module
 dir(logging)
-class Logging_Socratica:
+class LoggingLumberjack:
+    def __init__(self):
     #create and configure logger
-    logging.basicConfig(filename = "E:\\python\\Lumberjack.Log", level = logging.DEBUG) #basicconfig sets level to 30
+        self.logging.basicConfig(filename = "C:\\python\\Lumberjack.Log", level = logging.DEBUG) #basicconfig sets level to 30
     #if file does not exist python will create it
     
-    logger = logging.getLogger()
+        self.logger = logging.getLogger()
     #working with one logger without a name. Known as root logger.
     
     #Test the logger
-    logger.info("Our first info message.")
+        self.logger.info("Our first info message.")
     
-    print(logger.level)
+        print(logger.level)
     
     #There are six preset levels
     #NotSet 0
@@ -27,6 +28,16 @@ class Logging_Socratica:
     #ERROR 40
     #CRITICAL 50
     
-    if __name__ == __main__:
-        new_logging = Logging_Socratica()
-        print(new_logging)
+    #need to input the time that the log was created
+    
+    class EnragedLumber:
+        def __init__(self):
+            self.LOG_FORMAT= "%(levelname)s %asctime)s - %(message)"
+            self.logging.basicConfig(filename = "C:\\python\\EnragedLumberjack.Log", 
+                            level = logging.DEBUG, 
+                            format = LOG_FORMAT)
+    
+    if __name__ == "__main__":
+        new_logging_lumber = LoggingLumberjack()
+        new_logging_enraged = EnragedLumber()
+        print("Logging intialized for both classes")
