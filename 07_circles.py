2b00e7c8884a59da019800f06c3ce12253330e72
dhelms33 <derrickhelms10@gmail.com>
2025-08-28 18:27:20 -0600

    Added more unit tests and functions

--- 07_circles.py
+++ 07_circles.py
@@ -2,6 +2,10 @@ from math import pi
 
 """"""
 def calc_area(r):
+    if type(r) not in [int, float]:
+        raise TypeError("The radius must be a non-negative real number.")
+    if r < 0:
+        raise ValueError*===("The radius cannot be negative.")
     return pi*(r**2)
 
 #test function
--- unittests/07_test_circles.py
+++ unittests/07_test_circles.py
@@ -6,7 +6,16 @@ class TestsCircleArea(unnittest.TestCase):
         self.assertAlmostEqual(calc_area(1), pi)
         self.assertAlmostEqual(calc_area(0), 0)
         self.assertAlmostEqual(calc_area(2.1), pi* 2.1**2)
+        #not sure if correct
+        self.assertRaises(calc_area(-2), ValueError)
         
     def test_values(self):
         #Make sure value errors are raised when necessary
         self.assertRaises(ValueError, calc_area, -2)
+#if you want to learn morea bout py, use help text in interative mode
+# import unittest
+help(unittest.TestCase.assertSetEqual)
+    def test_types(self):
+        self.assert(TypeError, calc_area, 3+5j)
+        self.assert(TypeError, calc_area, True)
+        self.assert(TypeError, calc_area, "radius")
