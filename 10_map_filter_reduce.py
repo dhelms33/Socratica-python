472949b13f7cd456bd0c25b9f9c10c8fde42fb9a
dhelms33 <derrickhelms10@gmail.com>
2025-09-08 12:34:04 -0600

    Added map function

---
+++ 10_map_filter_reduce.py
@@ -0,0 +1,15 @@
+import math
+
+def area(r):
+    return math.pi * (r**2)
+
+#method 1: direct
+
+areas = [] 
+for r in radii:
+    a = area(r)
+    areas.append(a)
+    
+#method 2 map
+map_object = map(area, radii)
+list_map = list((map_object))
