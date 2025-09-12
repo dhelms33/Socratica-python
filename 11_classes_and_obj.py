9904e960e4477e77402d6470d8dc63274be06890
dhelms33 <derrickhelms10@gmail.com>
2025-09-11 23:15:04 -0600

    Added classes

---
+++ 11_classes_and_obj.py
@@ -0,0 +1,25 @@
+#left off 3:37
+#make a class that does nothing
+class User:
+    pass
+
+if __name__ == "__main__":
+    user1 = User()
+    #user1 is an instance or object 
+    user1.first_name = "Dave"
+    user1.last_name = "Boogeyman"
+    #these are fields
+    print(user1.first_name)
+    print(user1.last_name)
+    
+    #stand alone variables are not attached or associated with user1
+    first_name = "Arthur"
+    last_name = "Clarke"
+    
+    #values kept seperate
+    user2 = User()
+    user2.first_name = "Frank"
+    user2.last_name = "Poop"
+    
+    
+    
