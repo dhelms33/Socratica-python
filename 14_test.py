2d180ed28def454d6007c4f9311e60a07436f392
dhelms33 <derrickhelms10@gmail.com>
2025-10-26 04:35:03 -0600

    Added unit tests

--- 14_test.py
+++ 14_test.py
@@ -4,6 +4,20 @@ from math import pi
 
 class TestCircleArea(unittest.TestCase):
     def test_area(self):
+        # test areas when radius >= 0
         self.assertAlmostEqual(1), pi)
         self.assertAlmostEqual(0),0)
         self.assertAlmostEqual(2.1), pi * 2.1**2)
+    
+    def test_values(self):
+        # Make sure value errors are raised when necessary
+        self.assertRaises(ValueError, circle_area, -2)
+    
+    def test_types(self):
+        #Make sure type errors are raised
+        self.assertRaises(TypeError, circle_area, 3+5j)
+        self.assertRaises(TypeError, circle_area, True)
+        self.assertRaises(TypeError, circle_area, "String")
+        
+        
+        
--- 15_test_pi.py
+++ 15_test_pi.py
@@ -2,9 +2,14 @@
 #python -m unit test test_circles
 # python -m unittest
 #automatically finds py tests
+from math import pi
 
 def circle_area(r):
-    return pi(r**2)
+    if type(r) not in [int, float]:
+        raise TypeError("The raidus must be a non-negative real number")
+    if r < 0:
+        raise ValueError("Value cannot be negative.")
+    return pi*(r**2)
 
 #test function 
 radii =[2, 0, -3, 2+5j, True, "radius"]
@@ -13,4 +18,9 @@ message = "Area of circles with {radius} is {area}."
 for r in radii:
     A = circle_area(r)
     print (message.format(radius=r, area=A))
+
+#look at help text to learn python
+import unittest
+help(unittest.TestCase.assertSetEqual)
+
 
