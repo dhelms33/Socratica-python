6c41301ce6086d06035c7131a6a8f1ce24e47d6e
dhelms33 <derrickhelms10@gmail.com>
2025-11-03 20:52:59 -0700

    Added new object compare class

---
+++ 17_booleans.py
@@ -0,0 +1,11 @@
+class ObjectCompare:
+    def __init__(self, ui):
+        self.ui = ui
+        
+        
+        #type(28)
+        #type(True)
+        #tells us type
+        #bool("Turing") converts 
+    def convert_str_to_bool(ui:str)->bool:
+        return bool(ui)
