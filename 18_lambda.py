d1eba9ce0262d5e41c1a14cd019673a5d02bb950
Dereck Helms <74729959+dhelms33@users.noreply.github.com>
2025-11-05 16:06:37 -0700

    Create 18_lambda.py

---
+++ 18_lambda.py
@@ -0,0 +1,13 @@
+class Anon:
+    def __init__(self):
+        pass
+    lambda_fx= lambda x: 3*x+1
+
+    def mult_by_3_add_1(num:->int)->int:
+        return 3*num+1
+    def mult_by_6_add_
+   
+    if __name__ == "__main__":
+        ui= int(input("please input an int"))
+        obj = Anon()
+        ui_obj = obj.mult_by_3_add_1(ui)
