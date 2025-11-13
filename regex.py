21e3a3f697a9327594cfd7b626459d2df692550c
Dereck Helms <74729959+dhelms33@users.noreply.github.com>
2025-11-13 16:31:44 -0700

    Created regex.py

---
+++ regex.py
@@ -0,0 +1,7 @@
+import re
+class NameSearcher:
+    def__init__(self, name):
+        self.name = name
+    def name_matcher(self.name):
+        regex = "^"
+        return re.search(regex, self name)
