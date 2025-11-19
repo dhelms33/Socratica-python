acfef84eb5af66e89c0a45601f480263d8512230
dhelms33 <derrickhelms10@gmail.com>
2025-11-18 22:57:34 -0700

    Attempted to create a dict populated by user input

---
+++ 19_dictionaries.py
@@ -0,0 +1,8 @@
+class SpiffoFriendNetwork:
+    def __init__(self, id, message):
+        self.id = id
+        self.message = message
+    def create_dict(self.id, self.message):
+        for range in zip(self.id, self.message):
+            user_dict += {self.id:f"{self.message}"}
+        return user_dict
