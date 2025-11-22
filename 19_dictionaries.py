fefebe65e76e50c8ef49dbaaaf29eb4d2b3b5731
dhelms33 <derrickhelms10@gmail.com>
2025-11-22 00:19:04 -0700

    added dictionaries

--- 19_dictionaries.py
+++ 19_dictionaries.py
@@ -1,5 +1,10 @@
 #pass
 class SpiffoFriendNetwork:
+    post_og = {"user_id":209, "message":"D5 E5 C5 C4 G4", "language":"English", "datetime":"20230317","location":(44.590533, -104.715556)}
+
+    def __init__(self, user_id):
+        self.user_id = user_id
+        
     def get_post():
         post = {"user_id":209, "message":"D5 E5 C5 C4 G4", "language":"English", "datetime":"20230317","location":(44.590533, -104.715556)}
         return post
@@ -8,3 +13,15 @@ class SpiffoFriendNetwork:
     def get_post2():
         post2 = dict(message="SS Cotopaxi", language="English")
         return post2
+    def get_post3 (self.user_id, message):
+        post3 = dict(id = self.user_id, msg = message)
+        return post3
+    def get_location_dict(self.user_id, location):
+        location_dict = dict(id = self.user_id, loc = location)
+        return location_dict
+    def get_userid():
+        u_id_post_og = post_og["user_id"]
+        return u_id_post_og
+        
+    
+    
