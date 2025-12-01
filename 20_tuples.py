772584f99fa95c76f0a14625fb960997603d9a34
dhelms33 <derrickhelms10@gmail.com>
2025-12-01 00:47:25 -0700

    added tuples

--- 20_tuples.py
+++ 20_tuples.py
@@ -1,4 +1,4 @@
-#left off 2:22
+#left off 4:22
 import timeit
 class TuplesvLists:
     def __init__(self):
@@ -16,6 +16,14 @@ class TuplesvLists:
     def iterator_ex_l(prime_numbers):
         for item in prime_numbers:
             return("Prime numbers:", item)
+    def make_test_tuples():
+        empty_tuple = () 
+        test1 = ("a", )
+        test2 = ("a", "b")
+        test3 = ("a", "b", "c")
+        return(empty_tuple+test1+test2+test3)
+        
+        test
     #print("List methods")
     #print("tuples methods")
     #lists occupy more memory than tuples
@@ -27,7 +35,9 @@ class TuplesvLists:
         tuple_test = timeit.timeit(stmt = "(1,2,3,4,5)", number = 10000000)
         return tuple_test
 if __name__ == "__main__":
-    
+    obj_instance = TuplesvLists
+    tuples_count = obj_instance.get_list_prime()
+    print("tuples size ", tuples_count)
     
     #print(dir(sys))
     #print(help(sys.getsizeof))
