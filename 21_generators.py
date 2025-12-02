e7ff3384ead0cb5d5f33d9a66823849e7e5e7200
Dereck Helms <74729959+dhelms33@users.noreply.github.com>
2025-12-02 14:51:56 -0700

    Create 21_generators.py

---
+++ 21_generators.py
@@ -0,0 +1,13 @@
+class Tough:
+    def __init__(self, name):
+        self.name = name 
+    def fake_f():
+        return 1
+        return 2
+        return 3 #returns 1
+    def get_gen_obj():
+        yield 1
+        yield 2
+        yield 3
+for x in get_gen_obj():
+    print(x)
