8fd6b34ae416bbad28c56585d8a458d6105789f3
dhelms33 <derrickhelms10@gmail.com>
2025-12-03 14:49:22 -0700

    Added doom err logger

--- 04a_logging.py
+++ 04a_logging.py
@@ -4,7 +4,13 @@ class DoomFriends:
         self.name = name
         self.id = id
         self.message = message
+        
     def get_doom_logger():
-        logging.basicConfig(filename = "E:\\python\\DoomFriends.log")
+        logging.basicConfig(filename = "E:\\python\\DoomFriends.log", level = logging.DEBUG)
         logger = logging.getLogger()
         return logger.info("First Doom logger message")
+    
+    def get_doom_err_logger():
+        logging.basicConfig(filename="E:\\python\\DoomFriends.log", level = logging.ERROR, format = LOG_FORMAT)
+        logger_err = logging.getLogger()
+        return logger_err
