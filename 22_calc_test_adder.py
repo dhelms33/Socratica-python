63d6ea6c644cffd49dbb9fb2c96bbf036fb22d2c
dhelms33 <derrickhelms10@gmail.com>
2025-12-05 18:09:56 -0700

    Added Test_adder

--- 22_calc_test_adder.py
+++ 22_calc_test_adder.py
@@ -1,6 +1,6 @@
 import unittest
+from 23_TestAdder import half_adder
 class TestAdder:
-    def __init__(self,a,b,input):
-        self.assertAlmostEqual(carry_adder(a,b), 1,1)
-        self.b = b
-    def test_
+    def test__area(unittest.TestCase):
+        self.assertAlmostEqual(half_adder(1,1), carry = True, 0)
+        self.assertAlmostEqual(half_adder(1,0), carry = False, 1)
