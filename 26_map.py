e3846ae8f14ede1dd49716d747798d25fef8a323
dhelms33 <derrickhelms10@gmail.com>
2026-02-02 13:40:35 -0700

    Added conv_city_temp_c

--- 26_map.py
+++ 26_map.py
@@ -1,3 +1,12 @@
 class TemperatureMap:
     def __init__(self, temps):
         temps = [("Berlin", 29), ("Cairo", 36), ("Buenos Aires", 19)]
+    
+    def c_to_f(data[0], data[1]):
+        conversion = (9/5) * data[1] + 32
+        return(data[0], conversion)
+    lambda data: (data[0], (9/5)*data[1]+32)
+    
+    def conv_city_temp_C(c_to_f, temps):
+        list_mapped_values = list(map(c_to_f, temps))
+        return(list_mapped_values)
