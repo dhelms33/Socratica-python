469d5e33395803dcbdc635a10dec907c3faf63a6
dhelms33 <derrickhelms10@gmail.com>
2026-02-06 00:43:36 -0700

    Added create alt_temps

--- 26_map.py
+++ 26_map.py
@@ -1,6 +1,19 @@
 class TemperatureMap:
-    def __init__(self, temps):
-        temps = [("Berlin", 29), ("Cairo", 36), ("Buenos Aires", 19)]
+    def __init__(self, temp, ui):
+        self.temp = temp
+        self.ui = ui
+        
+    def add_5C(ui):
+        result = ui + 5
+        return(result)
+        
+    temps = [("Berlin", 29), ("Cairo", 36), ("Buenos Aires", 19)]
+    def create_alt_temps(self.ui):
+        array_temps = []
+        for ui in self.ui:
+            inp = add_5c(ui)
+            array_temps.append(inp)
+        return(array_temps)
     
     def c_to_f(data[0], data[1]):
         conversion = (9/5) * data[1] + 32
@@ -10,3 +23,8 @@ class TemperatureMap:
     def conv_city_temp_C(c_to_f, temps):
         list_mapped_values = list(map(c_to_f, temps))
         return(list_mapped_values)
+    
+    if __name__ == "__main__":
+        try:
+            user_input = int(input("Please input a celsius temperature: "))
+            obj_instance = TemperatureMap()
