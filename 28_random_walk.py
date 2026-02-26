942619fe16003d7a0c6362fbd50fff06d89b1de6
dhelms33 <derrickhelms10@gmail.com>
2026-02-25 23:57:27 -0700

    Added random_walk

---
+++ 28_random_walk.py
@@ -0,0 +1,12 @@
+class Walking:
+    def random_walk(n):
+        x = 0
+        y = 0
+        for i in range(n):
+            step = random.choice(['N','S','E','W'])
+            if step == N:
+                y +=1
+            elif: 
+                pass
+            #TODO implement elif
+        return step
