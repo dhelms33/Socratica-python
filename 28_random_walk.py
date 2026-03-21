c0221e712fd169a28c40264ed24040032897b0dc
dhelms33 <derrickhelms10@gmail.com>
2026-03-20 18:06:54 -0600

    Added sets

--- 28_random_walk.py
+++ 28_random_walk.py
@@ -1,12 +1,17 @@
 class Walking:
     def random_walk(n):
+        """Return coordinate after n block random walk"""
         x = 0
         y = 0
         for i in range(n):
             step = random.choice(['N','S','E','W'])
-            if step == N:
+            if step == 'N':
                 y +=1
-            elif: 
-                pass
+            elif step == 'S':
+                x += 1
+            elif step == 'E':
+                x -=1
+            else:
+                y-=1
             #TODO implement elif
         return step
---
+++ 29_sets.py
@@ -0,0 +1,21 @@
+class Jojo:
+    def __init__(self, volume, name):
+        self.volume = volume
+        self.name = name
+    
+    def create_set_manga():
+        """ Creates a set of manga for JJBA """
+        manga = set()
+        #dir(manga) provides a reminder on how to use set
+        manga.add("Stell Ball Run")
+        manga.add(True)
+        manga.add("Volume")
+        manga.add(1)
+        #can add different elements
+        #sets doesn't order elements, doesn't contain duplicates
+        #len(manga)
+        #help(manga.remove)
+        #can remove elements
+        #2:46
+        return manga
+    
