10895e29d19815844380e2417e589e70d3a3a3bd
dhelms33 <derrickhelms10@gmail.com>
2026-03-22 00:33:45 -0600

    Added elements

--- 29_sets.py
+++ 29_sets.py
@@ -11,6 +11,10 @@ class Jojo:
         manga.add(True)
         manga.add("Volume")
         manga.add(1)
+        manga.remove(1)
+        #.remove can throw an error if object not in set
+        manga.discard(34)
+        #does not return anything
         #can add different elements
         #sets doesn't order elements, doesn't contain duplicates
         #len(manga)
@@ -18,4 +22,24 @@ class Jojo:
         #can remove elements
         #2:46
         return manga
+    
+    def create_set_anime():
+        anime = set([1, True, "Phantom Blood", "Volume"])
+        anime_copy = set([2, True, "Battle Tendency"])
+        anime_copy.clear()
+        return anime
+    def create_union():
+        obj_init = create_set_manga()
+        obj_2_init = create_set_anime()
+        return obj_init.union(obj_2_init)
+    def create_intersection():
+        obj_init = create_set_manga()
+        obj_2_init = create_set_anime()
+        return obj_init.intersection(obj_2_init)
+    def test_cases():
+        obj_init = create_set_manga()
+        obj_2_init = create_set_anime()
+        test_1 = 2 in obj_init
+        return test_1
+    
     
