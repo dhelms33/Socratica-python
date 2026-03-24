091f315605a3e39f6f4b0fb2ce580ea52d5fba49
dhelms33 <derrickhelms10@gmail.com>
2026-03-23 18:08:09 -0600

    Added create squares and bugfix

--- 30_List_Comprehension.py
+++ 30_List_Comprehension.py
@@ -4,13 +4,15 @@ class MathyMath:
     
     def create_squares(num):
         squares = []
-        if num > 1:
+        if num < 1:
             return("Error! Please try a number greater than 1")
         for item in range(1, num):
             squares.append(i**2)
         return squares
     
     def create_simple_sq(num):
+        if num < 1:
+            return("Error! Please try a number greater than 1")
         squares2 = [i**2 for i in range(1, num)]
         return squares2
     #left off 2:16
