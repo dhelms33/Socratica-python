53ec10b771ce78b495cbd4509cacc0ac4583864f
dhelms33 <derrickhelms10@gmail.com>
2026-03-25 17:21:12 -0600

    Added lists

---
+++ 31_lists.py
@@ -0,0 +1,16 @@
+class Primer:
+    def __init__(self, num):
+        self.num = num
+    def get_primes():
+        return self.num
+    def create_primes():
+        primes = [2,3,5,7,11,13]
+        primes.append(17)
+        primes.append(19)
+        return primes
+    
+    def create_custom_primes(primes:list)->list:
+        primes.append(17)
+        return primes
+    
+    
